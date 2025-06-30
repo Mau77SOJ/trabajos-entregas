2e0a1ccc4c0ef2a1ceb6395b51840c8e5af35478
Mau77SOJ <mauronicolas.86263@gmail.com>
2025-06-30 16:09:04 -0300

    Delete Analisis Audio/src/main2.py

--- Analisis Audio/src/main2.py
+++
@@ -1,8 +0,0 @@
-# Leer el mismo audio con diferente tasa
-from IPython.display import Audio
-
-print("Reproducción rápida:")
-Audio(audio, rate=32000)  # más agudo y rápido
-
-print("Reproducción lenta:")
-Audio(audio, rate=8000)  # más grave y lento
